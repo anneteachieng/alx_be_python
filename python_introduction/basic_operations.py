5a13f87b33c909293390e3b427a161492be68822
Annete <achiengomondi02@gmail.com>
2025-05-20 22:02:13 +0300

    addition, subtraction and multiplication of 2 numbers

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,21 @@
+#defining variables
+
+number1 = 10
+number2 = 5
+
+print("addition of", number1, "and", number2, "is", number1 + number2)
+
+#subtraction
+
+number1 = 10
+number2 = 5
+
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+
+#multiplication
+
+number1 = 10
+number2 = 5
+
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
+
