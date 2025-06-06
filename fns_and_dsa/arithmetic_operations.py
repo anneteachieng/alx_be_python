6dc19878a80fe74b6384d49c94efee1b080fb31a
anneteachieng <achiengomondi02@gmail.com>
2025-06-07 00:19:04 +0300

    basic arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,20 +7,20 @@ def perform_operation(num1, num2, operation):
     operation = str(input("Enter the operation: (add, subtract, multiply, divide.): "))
 
 
-    match operation:
-        case "add":
-            return num1 + num2
+    if operation == "add":
+        return num1 + num2
 
-        case "subtract":
-            return num1 - num2
+    elif operation == "subtract":
+        return num1 - num2
 
-        case "multiply":
-            return num1 * num2
+    elif operation ==  "multiply":
+        return num1 * num2
 
-        case "divide":
-            if num2 == 0:
-                return "Cannot divide by 0."
-            else:
-                return num1 / num2
-        case _:
-            return "Error: Invalid operation."
+    elif operation == "divide":
+        if num2 == 0:
+            return "Cannot divide by 0."
+        else:
+            return num1 / num2
+
+    else:
+        return "Error: Invalid operation."
