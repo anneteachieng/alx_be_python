8a6fb73018b37b4cf1bb6b7bbfb3329b6e190b77
anneteachieng <achiengomondi02@gmail.com>
2025-06-07 19:31:23 +0300

    future date using time delta and datetime module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,10 +1,10 @@
 #current date and time
 
-import datetime
+from datetime import datetime, timedelta
 
-current_date = datetime.date.today()
+current_date = datetime.now()
 days_to_add = int(input("Enter the number of days to add to the current date: "))
-calculate_future_date = datetime.timedelta(days=days_to_add)
+calculate_future_date = timedelta(days=days_to_add)
 future_date = current_date + calculate_future_date
 
 print(future_date)
