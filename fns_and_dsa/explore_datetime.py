294981fbbd866b16c3b1e227c0f50acc90d374f6
anneteachieng <achiengomondi02@gmail.com>
2025-06-07 19:12:18 +0300

    calculating future date using datetime module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,4 +3,7 @@
 import datetime
 
 current_date = datetime.date.today()
+calculate_future_date = datetime.timedelta(int(input("Enter the number of days to add to the current date: ")))
+future_date = current_date + calculate_future_date
 
+print(future_date)
